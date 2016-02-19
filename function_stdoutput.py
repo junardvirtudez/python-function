de79594ead1a96ff95c0e614217b981eb638ec14
Junard Virtudez <virtudezjunard@gmail.com>
2016-02-19 04:49:32 -0500

    changed function name from MyFunc to Funcount

--- function_stdoutput.py
+++ function_stdoutput.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-def Myfunc(input1,input2):
+def Funcount(input1,input2):
         print "Hi! My name is",input1+" and my age is",input2
         print "Hi! My name is %s and my age is %s"%(input1,input2)
         print "Hi! My name is {} and my age is {}".format(input1,input2)
         
-Myfunc("Junard",19)    
+Funcount("Junard",19)    
 
 
 
