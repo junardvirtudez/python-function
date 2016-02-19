d80ee967299c5af0ff57e76398bc53385e08af6a
Junard Virtudez <virtudezjunard@gmail.com>
2016-02-19 03:57:46 -0500

    Added python script that accepts name and age

---
+++ function_stdoutput.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+input1 = raw_input("Enter name: ")
+input2 = input("Enter age: ")
+
+print "Hi! My name is "+input1+" and my age is",input2
+print "Hi! My name is %s and my age is %s"%(input1,input2)
+print "Hi! My name is",input1,"and my age is",input2
+
+
+
